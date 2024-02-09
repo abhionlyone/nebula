62f1b15581b4cd7dee11ce93f537a43dc5619940
Abhi Reddy <abhilashmv@outlook.com>
2024-02-10 03:18:48 +0530

    Fix logout and custom JS/CSS issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.dartsass.builds = {
-  "custom.scss"  => "custom.css",
+  "styles.scss"  => "styles.css",
 }
